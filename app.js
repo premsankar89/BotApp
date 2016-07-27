23424e8866f8d8da72ca71bbc21638535de8211c
premsankar89 <palwarsa@andrew.cmu.edu>
2016-07-27 10:05:00 -0400

    Update app.js

--- app.js
+++ app.js
@@ -56,6 +56,26 @@ app.listen(appEnv.port, '0.0.0.0', function() {
   console.log("server starting on " + appEnv.url);
 });
 
+connector = new builder.ChatConnector({
+    appId: microsoft_app_id,
+    appPassword: microsoft_app_pswd
+});
+msbot = new builder.UniversalBot(connector);
+
+msbot.dialog('/', function (session) {
+ var payload = {
+    workspace_id: conv_workspace_id,
+    input: {"text":session.message.text},
+    context: {}
+  };
+  conversation.message(payload, function(err, data) {
+    if (err) {
+      console.log(err);
+    }
+    else
+      session.send('I understood your intent was:'+data.intents[0].intent);
+  });
+});
 //Configure all token info here
 app.post('/configure', function (req, res) {
    //@sputhana add logging here
@@ -93,21 +113,6 @@ connector = new builder.ChatConnector({
     appPassword: microsoft_app_pswd
 });
 msbot = new builder.UniversalBot(connector);
-
-msbot.dialog('/', function (session) {
- var payload = {
-    workspace_id: conv_workspace_id,
-    input: {"text":session.message.text},
-    context: {}
-  };
-  conversation.message(payload, function(err, data) {
-    if (err) {
-      console.log(err);
-    }
-    else
-      session.send('I understood your intent was:'+data.intents[0].intent);
-  });
-});
 }
 
 function configureConversation() {
